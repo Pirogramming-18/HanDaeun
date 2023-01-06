6004bcb304906b7be6d724c1d4092da8cc8bbbbc
daeun han <daeunhan0723@gmail.com>
2023-01-06 12:46:48 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,31 +1,43 @@
 num = 0
+start = 0
 
 while(True):
-    try:
-        numm = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        if (not(1 <= numm <= 3)):
-            raise Exception()
-    except ValueError:
-        print('정수를 입력하세요')
-    except Exception:
-        print('1,2,3 중 하나를 입력하세요')
-    else:
-        break
+    while(True):
+        try:
+            numm = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            if (not(1 <= numm <= 3)):
+                raise Exception()
+        except ValueError:
+            print('정수를 입력하세요')
+        except Exception:
+            print('1,2,3 중 하나를 입력하세요')
+        else:
+            break
 
-for num in range(numm):
-    print("playerA :", num+1)
+    for num in range(start, start+numm):
+        print("playerA :", num+1)
+        start += 1
+        if (start >= 31):
+            break
+    if (start >= 31):
+            break
 
-while(True):
-    try:
-        numn = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        if (not(1 <= numn <= 3)):
-            raise Exception()
-    except ValueError:
-        print('정수를 입력하세요')
-    except Exception:
-        print('1,2,3 중 하나를 입력하세요')
-    else:
-        break
+    while(True):
+        try:
+            numn = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            if (not(1 <= numn <= 3)):
+                raise Exception()
+        except ValueError:
+            print('정수를 입력하세요')
+        except Exception:
+            print('1,2,3 중 하나를 입력하세요')
+        else:
+            break
 
-for num in range(numm,numm+numn):
-    print("playerB :", num+1)
+    for num in range(start, start+numn):
+        print("playerB :", num+1)
+        start += 1
+        if (start >= 31):
+            break
+    if (start >= 31):
+            break
