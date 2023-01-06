0be503b77719d1fa109cd7cbfca99c3836a870dd
daeun han <daeunhan0723@gmail.com>
2023-01-06 12:50:55 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,19 +1,23 @@
 num = 0
 start = 0
 
-while(True):
+def brGame():
     while(True):
         try:
             numm = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
             if (not(1 <= numm <= 3)):
-                raise Exception()
+                raise Exception()        
         except ValueError:
             print('정수를 입력하세요')
         except Exception:
             print('1,2,3 중 하나를 입력하세요')
         else:
             break
-
+    return numm
+    
+while(True):
+    numm = brGame()
+    
     for num in range(start, start+numm):
         print("playerA :", num+1)
         start += 1
@@ -22,20 +26,10 @@ while(True):
             break
     if (start >= 31):
             break
+        
+    numm = brGame()
 
-    while(True):
-        try:
-            numn = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-            if (not(1 <= numn <= 3)):
-                raise Exception()
-        except ValueError:
-            print('정수를 입력하세요')
-        except Exception:
-            print('1,2,3 중 하나를 입력하세요')
-        else:
-            break
-
-    for num in range(start, start+numn):
+    for num in range(start, start+numm):
         print("playerB :", num+1)
         start += 1
         if (start >= 31):
@@ -43,3 +37,4 @@ while(True):
             break
     if (start >= 31):
             break
+
